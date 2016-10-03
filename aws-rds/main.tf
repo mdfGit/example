c0482e7063aeb7c236113dd96557dd443a624df6
Mark Ferry <mark_ferry@harvard.edu>
2016-10-03 17:12:21 -0400

    first commit

---
+++ aws-rds/main.tf
@@ -0,0 +1,19 @@
+resource "aws_db_instance" "default" {
+  depends_on             = ["aws_security_group.default"]
+  identifier             = "${var.identifier}"
+  allocated_storage      = "${var.storage}"
+  engine                 = "${var.engine}"
+  engine_version         = "${lookup(var.engine_version, var.engine)}"
+  instance_class         = "${var.instance_class}"
+  name                   = "${var.db_name}"
+  username               = "${var.username}"
+  password               = "${var.password}"
+  vpc_security_group_ids = ["sg-f326d888"]
+  db_subnet_group_name   = ["subnet-c4c2b9ef"]
+}
+
+resource "aws_db_subnet_group" "default" {
+  name        = "main_subnet_group"
+  description = "Our main group of subnets"
+  subnet_ids  = ["subnet-c4c2b9ef", "subnet-463d0231"]
+}
