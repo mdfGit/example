f39c731b045205e37f0c249e987eee0d32b99888
Mark Ferry <mark_ferry@harvard.edu>
2016-10-04 16:22:42 -0400

    updated with provider data

--- aws-rds/main.tf
+++ aws-rds/main.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAJ4JWNSJBS7A7O3TA"
+  secret_key = "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"
+}
+
 resource "aws_db_instance" "default" {
   identifier             = "${var.identifier}"
   allocated_storage      = "${var.storage}"
@@ -16,3 +22,4 @@ resource "aws_db_subnet_group" "default" {
   description = "Our main group of subnets"
   subnet_ids  = ["subnet-c4c2b9ef", "subnet-463d0231"]
 }
+
